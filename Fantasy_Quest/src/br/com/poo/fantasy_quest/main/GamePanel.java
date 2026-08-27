7a88619cefd4992e30756bba06b07ad12dcb4a6e
inctr <inctr@UFC>
2026-08-27 13:39:10 -0300

    feat: implementing a game loop using the delta accumulator approach

--- Fantasy_Quest/src/br/com/poo/fantasy_quest/main/GamePanel.java
+++ Fantasy_Quest/src/br/com/poo/fantasy_quest/main/GamePanel.java
@@ -20,13 +20,45 @@ public class GamePanel extends JPanel implements Runnable{
 	final int screenWidth = tileSize * maxScreenCol;  //768 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 	
+	
+	//FPS
+	int FPS = 60;
+	
+    /**
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+     *  :::::::::::::::: KeyHandler ::::::::::::::::::::::
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+    **/
+	KeyHandler keyH = new KeyHandler();
+	
+    /**
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+     *  :::::::::::::::: GAME THREAD :::::::::::::::::::::
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+    **/
 	Thread gameThread;
 	
+	
+	//Set player's default position
+	int playerX     = 100;
+	int playerY     = 100;
+	int playerSpeed = 4;
+	
+    /**
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+     *  :::::::::::::::: GAME PANEL :::::::::::::::::::::
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+    **/
 	public GamePanel()
 	{
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		
+		// key input
+		this.addKeyListener(keyH);
+		this.setFocusable(true); // <- Game Panel can be "focused" to receive key input 
+	
 	}
 	
 	public void startGameThread()
@@ -36,8 +68,25 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void update()
-	{
+	{	
+		// update char position
 		
+		if(keyH.upPressed == true)
+		{
+			playerY -= playerSpeed;
+		}
+		else if(keyH.downPressed == true)
+		{
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true)
+		{
+			playerX -= playerSpeed;
+		}
+		else if(keyH.rightPressed == true)
+		{
+			playerX += playerSpeed;
+		}
 	}
 	
 	public void paintComponent(Graphics g)
@@ -48,21 +97,98 @@ public class GamePanel extends JPanel implements Runnable{
 		Graphics2D g2 = (Graphics2D)g;
 		
 		g2.setColor(Color.white);
-		g2.fillRect(100, 100, tileSize, tileSize);
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
 		g2.dispose();
 	}
 	
-	// The Game Loop! Ebaaa
-	@Override
+	//The Game Loop - Abordagem delta accumulator - balde de tempo
+	
+    /**
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+     *  :::::::::::::::: GAME LOOP  ::::::::::::::::::::::
+     *  ::::::::::::::::::::::::::::::::::::::::::::::::::
+    **/
+	
+	@Override 
 	public void run()
-	{
-		while(gameThread != null)
-		{
-			// 1. UPDATE: update information such as character positions
-			update();
+	{	
+		double drawInterval = 1_000_000_000/FPS; //16,66 ms
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		long timer = 0;
+		int drawCount = 0;
+		
+		while (gameThread != null)
+		{	
+			currentTime = System.nanoTime();
 			
-			// 2. DRAW: draw the screen with the updated information
-			repaint(); // -- calls paintComponent method
+			delta += (currentTime - lastTime) / drawInterval;
+			
+			//
+			timer += (currentTime - lastTime);
+			
+			lastTime = currentTime;
+			
+			if (delta >= 1)
+			{
+				// 1. UPDATE: update information such as character positions
+				update();
+				
+				// 2. DRAW: draw the screen with the updated information
+				repaint(); // -- calls paintComponent method	
+				
+				delta--;
+				
+				drawCount++; // increase drawCounts!
+			}
+			
+			if (timer >= 1_000_000_000) {System.out.println("FPS: " + drawCount); drawCount = 0; timer = 0;}
 		}
 	}
+	
+
+//Ctrl + Shift + C --> Retira comentários
+// 
+//	The Game Loop - Abordagem Sleep
+//	@Override
+//	public void run()
+//	{
+//		while(gameThread != null)
+//		{	
+//					
+//			//Sleep Method - fix FPS to 60
+//			//ps: this means we draw the screen every 0.01666 seconds
+//			double drawInterval = 1_000_000_000/FPS; // nanoseconds/fps
+//			double nextDrawTime = System.nanoTime() + drawInterval;
+//			
+//			// 1. UPDATE: update information such as character positions
+//			update();
+//			
+//			// 2. DRAW: draw the screen with the updated information
+//			repaint(); // -- calls paintComponent method
+//			
+//			try 
+//			{
+//				double remainingTime = nextDrawTime - System.nanoTime();
+//				remainingTime /= 1_000_000; // nano -> milisseconds
+//				
+//				if(remainingTime < 0)
+//				{
+//					remainingTime = 0;
+//				}
+//				
+//				Thread.sleep((long)remainingTime);
+//				
+//				nextDrawTime += drawInterval;
+//			} 
+//			catch (InterruptedException e) 
+//			{
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//		}
+//	}
 }
--- Fantasy_Quest/src/br/com/poo/fantasy_quest/package-info.java
+++
@@ -1,1 +0,0 @@
-package br.com.poo.fantasy_quest;
